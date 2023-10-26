a0e88d82ee7e3bfe4e85252f416e60aaeec8a2b6
Ankit Kumar <2041011129.ankitkumar@gmail.com>
2023-10-26 14:16:55 +0530

    change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Ankit Kumar is 
+          Ankit Kumar is DESCIPLINED PERSON!
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello React
+          Namestay React
         </a>
       </header>
     </div>
